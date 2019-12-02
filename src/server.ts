7648f920361b2e1faa0a8b54b117496806adbb82
JakeMaldonado <jakemaldonado@protonmail.com>
2019-12-02 12:01:23 +0100

    fix server breaking from missing env.PORT

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import App from './app';
 import StreaksController from './controllers/streaks/streak.controller';
 import UsersController from './controllers/users/user.controller';
 
-const port = process.env || 3000;
+const port = process.env.PORT || 3000;
  
 const app = new App(
   [
