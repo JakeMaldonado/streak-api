ba05b27906ffdfdedcb5db762e83487d22d5409d
JakeMaldonado <jakemaldonado@protonmail.com>
2019-12-07 18:51:56 +0100

    add uuid

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,13 @@
 import * as mongoose from "mongoose";
 
 export interface iUser extends mongoose.Document {
+  userId: string;
   username: string; 
   password: string;
 }
 
 export const UserSchema = new mongoose.Schema({
+  userId: {type: String, required: true},
   username: {type: String, required: true},
   password: {type: String, required: true},
 });
